6d25fc2910154d3cf2c5a5c59b41446657dd75cd
Gabriel de Freitas <gabrielfsantos.ti@gmail.com>
2024-08-04 17:42:25 -0300

    criando tela de cadastro de usuario

--- lib/Paginas/cadastroPage.dart
+++ lib/Paginas/cadastroPage.dart
@@ -1,2 +1,151 @@
+import 'package:adota_pet_front/Paginas/listaUsuariosPage.dart';
+import 'package:flutter/material.dart';
+import 'package:adota_pet_front/Api/api.dart';
 
+//Tela ok por enquanto
+
+class cadastroPage extends StatefulWidget {
+  const cadastroPage({super.key});
+
+  @override
+  State<cadastroPage> createState() => cadastroPageState();
+}
+
+class cadastroPageState extends State<cadastroPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _nickController = TextEditingController();
+  final TextEditingController _telefoneController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+  final TextEditingController _confirmarSenhaController = TextEditingController();
+
+  Future<void> _criarUsuario() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      final bool sucesso = await api.cadastrarUsuario(
+        nome: _nomeController.text,
+        nick: _nickController.text,
+        telefone: _telefoneController.text,
+        senha: _senhaController.text,
+      );
+
+      if (sucesso) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Cadastro realizado com sucesso')),
+        );
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ListaDeUsuarios()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Falha ao cadastrar. Tente novamente')),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cadastro'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _nomeController,
+                decoration: const InputDecoration(
+                  labelText: 'Nome',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira seu nome';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _nickController,
+                decoration: const InputDecoration(
+                  labelText: 'Nick',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira seu nick';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _telefoneController,
+                decoration: const InputDecoration(
+                  labelText: 'Telefone',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira seu telefone';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _senhaController,
+                decoration: const InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira sua senha';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 24.0),
+              TextFormField(
+                controller: _confirmarSenhaController,
+                decoration: const InputDecoration(
+                  labelText: 'Confirmar Senha',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, confirme sua senha';
+                  } else if (value != _senhaController.text) {
+                    return 'As senhas não coincidem';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 24.0),
+              ElevatedButton(
+                onPressed: _criarUsuario, // Chama o método _register ao clicar no botão
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: const TextStyle(fontSize: 18),
+                ),
+                child: const Text('Cadastrar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
